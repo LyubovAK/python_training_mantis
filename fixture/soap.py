08bcd7310d1ca6f06c9dd8effdec80e1f41c22eb
Lyubov <Lyubov.Kovalevich@iitrust.ru>
2020-11-16 11:08:29 +0500

    В класс SoapHelper добавлено получение данных из json

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,6 +1,8 @@
 from suds.client import Client
 from suds import WebFault
 from model.project import Project
+import json
+import os.path
 
 
 class SoapHelper:
@@ -8,9 +10,17 @@ class SoapHelper:
     def __init__(self, app):
         self.app = app
 
-    def get_project_list(self, username="administrator", password="root"):
+    def get_project_list(self):
+        file_name = (os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'target.json'))
+        with open(file_name) as f:
+            info = json.load(f)
+        user = info["webadmin"]
+        username = user["username"]
+        password = user["password"]
+        url = info["web"]
+        name_url = url["baseUrl"]
         name_projects = []
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client("%s/api/soap/mantisconnect.php?wsdl" % name_url)
         try:
             projects = client.service.mc_projects_get_user_accessible(username, password)
             for element in projects:
@@ -19,3 +29,4 @@ class SoapHelper:
             return list(name_projects)
         except WebFault:
             return False
+
